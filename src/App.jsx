c166c4e34ddd9d691e4c44c79c531a91042f58e5
anastasia1sl <slobodyanik.a97@gmail.com>
2025-05-15 15:45:19 +0300

    normalize added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import "modern-normalize/modern-normalize.css";
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage/HomePage";
 import Header from "./components/Header/Header";
